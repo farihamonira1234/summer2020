aeca2de9f64d28d0951939a6a920240123ce2692
farriha monira <59191475+farihamonira1234@users.noreply.github.com>
2020-08-04 01:45:10 +0600

    Add files via upload
    modified

--- minilabtask/Login.php
+++ minilabtask/Login.php
@@ -7,21 +7,28 @@
 </head>
 
 <body>
-    <form action="registration_handler.php" method="POST">
-        <table>
-            Registration
-            <tr>
+    <form action="reg_handler.php" method="POST">
+      
+            <fieldset>
+             <legend>LOGIN</legend>
+               <table>
+                <tr>
                 <td>
                     <label for="id"> USER ID :</label>
-                    <input type="text" name="id" id="id" required>
+                    <input type="text" name="id" id="id" required><br>
+                </td>
+            </tr>
+            <tr>
                     <br>
                     <label for="password">Password :</label>
                     <input type="password" name="password" id="password" required>
                     <br>
-                    <input type="submit" name="submit" value="submit"></td>
+                </tr>
+                   <td> <input type="submit" name="submit" value="submit"><a href="">Register</a></td>
 
                 </tr>
             </table>
+        </fieldset>
         </form></body>
         
         </html>
